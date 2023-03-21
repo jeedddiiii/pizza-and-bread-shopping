1c6e70412724e68d19a43af9ee22e0346bc1b2c5
jeedddiiii <pendigon.once@gmail.com>
2023-03-22 02:33:18 +0700

    async

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,5 +1,8 @@
+import { useGetAllProductsQuery } from "../features/productApi";
+
 const Home = () => {
-    return ( <h2>Home</h2> );
-}
- 
+  const { data, error, isLoading } = useGetAllProductsQuery();
+  return <h2>Home</h2>;
+};
+
 export default Home;
--- client/src/index.js
+++ client/src/index.js
@@ -5,12 +5,18 @@ import App from "./App";
 import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 
-import productsReducer from "./features/productsSlice";
+import productsReducer, { productsFetch } from "./features/productsSlice";
+import { productsApi } from "./features/productApi";
 const store = configureStore({
   reducer: {
     products: productsReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
+
+store.dispatch(productsFetch());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
