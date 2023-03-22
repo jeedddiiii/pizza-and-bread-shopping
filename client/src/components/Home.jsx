c0b9fc3efa67b9b215598f41e7906ce65761cf23
jeedddiiii <pendigon.once@gmail.com>
2023-03-23 01:36:31 +0700

    display,style,addToCart

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,40 @@
-import { useGetAllProductsQuery } from "../features/productApi";
-
+import { useDispatch } from "react-redux";
+import { addToCart } from "../features/cartSlice";
+import { useGetAllProductsQuery } from "../features/productsApi";
 const Home = () => {
   const { data, error, isLoading } = useGetAllProductsQuery();
-  return <h2>Home</h2>;
+  const dispatch = useDispatch();
+  const handleAddToCart = (product) => {
+    dispatch(addToCart(product))
+  }
+  return (
+    <div className="home-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>An error occurred...</p>
+      ) : (
+        <>
+          <h2>New Arrivals</h2>
+          <div className="products">
+            {data?.map((product) => (
+              <div key={product.id} className="product">
+                <h3>{product.name}</h3>
+                <img src={product.image} alt={product.name} />
+                <div className="details">
+                  <span>{product.desc}</span>
+                  <span className="price">${product.price}</span>
+                </div>
+                <button onClick={() => handleAddToCart(product)}>
+                  Add to cart
+                </button>
+              </div>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Home;
--- server/products.js
+++ server/products.js
@@ -2,23 +2,23 @@ const products = [
   {
     id: 1,
     name: "Product 1",
-    description: "This is product 1",
+    desc: "This is product 1",
     price: 100,
-    img: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679349093/pizzabread/Milk-Bread-FF2_ja1yih.jpg",
+    image: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679349093/pizzabread/Milk-Bread-FF2_ja1yih.jpg",
   },
   {
     id: 2,
     name: "Product 2",
-    description: "This is product 2",
+    desc: "This is product 2",
     price: 200,
-    img: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679349094/pizzabread/french-bread-small-loaf-side-view-one-dish-kitchen-sq-1200_ehcyau.jpg",
+    image: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679349094/pizzabread/french-bread-small-loaf-side-view-one-dish-kitchen-sq-1200_ehcyau.jpg",
   },
   {
     id: 3,
-    name: "Product 3",
-    description: "This is product 3",
+    name: "Just a product",
+    desc: "puad lung",
     price: 300,
-    img: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679349094/pizzabread/french-bread-small-loaf-side-view-one-dish-kitchen-sq-1200_ehcyau.jpg",
+    image: "https://res.cloudinary.com/dpy0ss1we/image/upload/v1679479568/pizzabread/Square_200x200_yphie8.png",
   },
 ];
 
