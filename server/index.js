1dc4ba21eda1f4cde4a6d5b88e94c5d5963a61b6
jeedddiiii <pendigon.once@gmail.com>
2023-03-27 01:31:37 +0700

    Update connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,19 +1,32 @@
-const express = require('express');
-const cors = require('cors');
-const products = require('./products');
+const express = require("express");
+const cors = require("cors");
+const products = require("./products");
+const mongoose = require("mongoose");
 
 const app = express();
+
+require("dotenv").config();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {
-    res.send('Shop API');
-})
+app.get("/", (req, res) => {
+  res.send("Shop API");
+});
 
-app.get('/products', (req, res) => {
-    res.send(products);
-})
+app.get("/products", (req, res) => {
+  res.send(products);
+});
 
 const port = process.env.PORT || 5000;
-app.listen(port, console.log('server running on port ' + port));
+const uri = process.env.DB_URI;
+
+app.listen(port, console.log("server running on port " + port));
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("connected to MongoDB..."))
+  .catch((err) => console.log("MongoDB connection failed", err.message));
