597c50748ac6519ed8bd78ff0ae79dcb11f89422
jeedddiiii <pendigon.once@gmail.com>
2023-03-27 12:46:42 +0700

    Add login api

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const products = require("./products");
 const mongoose = require("mongoose");
 const register = require("./routes/register");
+const login = require("./routes/login");
 
 const app = express();
 
@@ -12,6 +13,8 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/api/register", register);
+app.use("/api/login", login);
+
 
 app.get("/", (req, res) => {
   res.send("Shop API");
