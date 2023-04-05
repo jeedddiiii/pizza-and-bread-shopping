ceb3f1869566727ca1ddaa7a9af3735a8708646c
jeedddiiii <pendigon.once@gmail.com>
2023-04-06 00:07:56 +0700

    Update stripe

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -8,6 +8,13 @@ const stripe = Stripe(process.env.STRIPE_KEY);
 const router = express.Router();
 
 router.post("/create-checkout-session", async (req, res) => {
+  const customer = await stripe.customers.create({
+    metadata: {
+      userId: req.body.userId,
+      cart: JSON.stringify(req.body.cartItems),
+    },
+  });
+
   const line_items = req.body.cartItems.map((item) => {
     return {
       price_data: {
@@ -54,6 +61,7 @@ router.post("/create-checkout-session", async (req, res) => {
       },
     ],
     phone_number_collection: { enabled: true },
+    customer: customer.id,
     line_items,
     mode: "payment",
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
@@ -62,5 +70,62 @@ router.post("/create-checkout-session", async (req, res) => {
 
   res.send({ url: session.url });
 });
+
+//Stripe Webhook
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+let endpointSecret;
+// endpointSecret =
+//   "whsec_7fad317042e130fb217e7a0bf8c08cc69c766a2f33fcb262e38f501257ee7330";
+
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (req, res) => {
+    const sig = req.headers["stripe-signature"];
+
+    let data;
+    let eventType;
+
+    if (endpointSecret) {
+      let event;
+
+      try {
+        event = stripe.webhooks.constructEvent(
+          req.body,
+          sig,
+          endpointSecret
+        );
+        console.log("Webhook received!");
+      } catch (err) {
+        console.log(`Webhook Error: ${err.message}`);
+        res.status(400).send(`Webhook Error: ${err.message}`);
+        return;
+      }
+
+      data = event.data.object;
+      eventType = event.type;
+    } else {
+      data = req.body.data.object;
+      eventType = req.body.type;
+    }
+
+    // Handle the event
+
+    if (eventType === "checkout.session.completed") {
+      stripe.customers
+        .retrieve(data.customer)
+        .then((customer) => {
+          console.log(customer);
+          console.log("data", data);
+
+        })
+        .catch((err) => console.log(err.message));
+    }
+
+    // Return a 200 res to acknowledge receipt of the event
+    res.send().end();
+  }
+);
 
 module.exports = router;
