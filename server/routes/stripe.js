9fad15a8bc90e7c385d89efd4add62cfa6b5c8c6
jeedddiiii <pendigon.once@gmail.com>
2023-04-05 21:16:10 +0700

    Update checkout shipping

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -1,41 +1,66 @@
-const express = require('express');
+const express = require("express");
 const Stripe = require("stripe");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const stripe = Stripe(process.env.STRIPE_KEY);
 
 const router = express.Router();
 
-router.post('/create-checkout-session', async (req, res) => {
-  try {
-    const session = await stripe.checkout.sessions.create({
-
-      line_items: [
-        {
-          price_data: {
-            currency: 'usd',
-            product_data: {
-              name: 'T-shirt',
-            },
-            unit_amount: 2000,
+router.post("/create-checkout-session", async (req, res) => {
+  const line_items = req.body.cartItems.map((item) => {
+    return {
+      price_data: {
+        currency: "usd",
+        product_data: {
+          name: item.name,
+          images: [item.image],
+          description: item.desc,
+          metadata: {
+            id: item.id,
           },
-          quantity: 1,
         },
-      ],
-      mode: 'payment',
-      success_url: `${process.env.CLIENT_URL}/checkout-success`,
-      cancel_url: `${process.env.CLIENT_URL}/cart`,
-    });
-  
-    res.send({url : session.url});
-    
-  } catch (error) {
-    console.log('kwai');
-    console.log(error);
-    
-  }
-  
+        unit_amount: item.price * 100,
+      },
+      quantity: item.cartQuantity,
+    };
+  });
+
+  const session = await stripe.checkout.sessions.create({
+    payment_method_types: ["card"],
+    shipping_address_collection: { allowed_countries: ["TH"] },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: { amount: 0, currency: "usd" },
+          display_name: "Free shipping",
+          delivery_estimate: {
+            minimum: { unit: "business_day", value: 5 },
+            maximum: { unit: "business_day", value: 7 },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: "fixed_amount",
+          fixed_amount: { amount: 1500, currency: "usd" },
+          display_name: "Next day air",
+          delivery_estimate: {
+            minimum: { unit: "business_day", value: 1 },
+            maximum: { unit: "business_day", value: 1 },
+          },
+        },
+      },
+    ],
+    phone_number_collection: { enabled: true },
+    line_items,
+    mode: "payment",
+    success_url: `${process.env.CLIENT_URL}/checkout-success`,
+    cancel_url: `${process.env.CLIENT_URL}/cart`,
+  });
+
+  res.send({ url: session.url });
 });
 
 module.exports = router;
