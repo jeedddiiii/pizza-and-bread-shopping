6748a873108034a3c16a55080ef48b00978d3007
jeedddiiii <pendigon.once@gmail.com>
2023-04-10 22:17:42 +0700

    Update styled

--- client/src/components/admin/Dashboard.jsx
+++ client/src/components/admin/Dashboard.jsx
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 import { Outlet, NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
+import {FaUsers,FaStore,FaClipboard,FaTachometerAlt} from 'react-icons/fa'
 
 const Dashboard = () => {
   const auth = useSelector((state) => state.auth);
@@ -17,7 +18,7 @@ const Dashboard = () => {
           }
           to="/admin/summary"
         >
-          Summary
+          <FaTachometerAlt/> Summary
         </NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -25,7 +26,7 @@ const Dashboard = () => {
           }
           to="/admin/products"
         >
-          Products
+          <FaStore/> Products
         </NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -33,7 +34,7 @@ const Dashboard = () => {
           }
           to="/admin/orders"
         >
-          Orders
+          <FaClipboard/> Orders
         </NavLink>
         <NavLink
           className={({ isActive }) =>
@@ -41,7 +42,7 @@ const Dashboard = () => {
           }
           to="/admin/users"
         >
-          Users
+         <FaUsers/> Users
         </NavLink>
       </SideNav>
       <Content>
@@ -79,6 +80,13 @@ const SideNav = styled.div`
     text-decoration: none;
     margin-bottom: 1rem;
     font-size: 14px;
+    display: flex;
+    align-items: center;
+    font-weight: 700;
+
+    svg {
+      margin-right: 0.5rem;
+      font-size: 18px;
   }
 `;
 
