c9e0322bd1e5d6b27c3e2afd6808e1c1105b513d
jeedddiiii <pendigon.once@gmail.com>
2023-04-10 20:43:00 +0700

    Update chart

--- client/src/components/admin/summary-components/Chart.jsx
+++ client/src/components/admin/summary-components/Chart.jsx
@@ -29,6 +29,7 @@ const Chart = () => {
 
   useEffect(() => {
     async function fetchData() {
+      setLoading(true);
       try {
         const res = await axios.get(`${url}/orders/week-sales`, setHeaders());
         res.data.sort(compare);
@@ -40,52 +41,50 @@ const Chart = () => {
           };
         });
         setSales(newData);
+        setLoading(false);
       } catch (err) {
         console.log(err);
+        setLoading(false);
       }
     }
     fetchData();
   }, []);
 
-  const data = [
-    {
-      day: "Mon",
-      amount: 4000,
-    },
-    {
-      day: "Tue",
-      amount: 3000,
-    },
-  ];
   return (
-    <StyledChart>
-      <h3>Last 7 Days Earnings(THB $)</h3>
-      <ResponsiveContainer width="100%" height="100%">
-        <LineChart
-          width={500}
-          height={300}
-          data={sales}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="day" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="amount"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
-    </StyledChart>
+    <>
+      {loading ? (
+        <Loader>Loading Chart ...</Loader>
+      ) : (
+        <StyledChart>
+          <h3>Last 7 Days Earnings(฿)</h3>
+          <ResponsiveContainer width="100%" height="100%">
+            <LineChart
+              width={500}
+              height={300}
+              data={sales}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="day" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Line
+                type="monotone"
+                dataKey="amount"
+                stroke="#8884d8"
+                activeDot={{ r: 8 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </StyledChart>
+      )}
+    </>
   );
 };
 
